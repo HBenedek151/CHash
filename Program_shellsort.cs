d7ab52f7e2cc4bc8156dd6d5886e63c2fd9d3527
HBenedek <horvath.benedek@diak.szbi-pg.hu>
2025-04-04 10:04:49 +0200

    Add files via upload

---
+++ Program_shellsort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shellsort
+{
+    internal class Program
+    {
+        static void ShellRendezés(int[] a)
+        {
+            int gap;
+            int n;
+            int j;
+            int x;
+            int y;
+            int i;
+            gap = 1;
+            n = a.Length;
+            while (gap * 2 <= n)
+            {
+                gap *= 2;
+            }
+            gap -= 1;
+            do
+            {
+                i = 0;
+                while ((i <= gap) && (i +gap < n))
+                {
+                    j = i + gap;
+                    while (j < n)
+                    {
+                        x = a[j];
+                        y = j - gap;
+                        while ((x > -1) && (x < a[y]))
+                        {
+                            a[y + gap] = a[y];
+                            y -= gap;
+                        }
+                        a[y + gap] = x;
+                        j += gap;
+                    }
+                    
+                }
+                gap = gap / 2;
+            } while (gap > 0);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] t = new int[10];
+            t[0] = 63;
+            t[1] = 54;
+            t[2] = 33;
+            t[3] = 45;
+            t[4] = 23;
+            t[5] = 99;
+            t[6] = 43;
+            t[7] = 10;
+            t[8] = 35;
+            t[9] = 87;
+            ShellRendezés(t);
+            for (int i = 0; i < 9; i++) Console.WriteLine(t[i]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cukraszda.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace cukraszda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverKapscolat = new MySqlConnectionStringBuilder { Server = "127.0.0.1", Database = "cukraszda", UserID = "root", Password = "" };
+            MySqlConnection kapcsolat = new MySqlConnection(serverKapscolat.ConnectionString);
+            kapcsolat.Open();
+            var lekerdezes = kapcsolat.CreateCommand();
+            Console.WriteLine("Hiányzó kalória érték: ");
+            lekerdezes.CommandText = "SELECT COUNT(*) AS \"Hiányzó kalória érték\"\r\nfrom termek\r\nWHERE termek.kaloria IS NULL;";
+            var olvaso = lekerdezes.ExecuteReader();
+            while (olvaso.Read())
+            {
+                Console.WriteLine($"{olvaso.GetInt64(0)} ");
+            }
+            olvaso.Close();
+            Console.WriteLine();
+            lekerdezes.CommandText = "SELECT DISTINCT termek.nev, kiszereles.mennyiseg from termek inner join kiszereles on termek.kiszerelesId = kiszereles.id WHERE kiszereles.mennyiseg LIKE \" % g\";";
+            olvaso = lekerdezes.ExecuteReader();
+            while(olvaso.Read())
+            {
+                Console.WriteLine($"{olvaso.GetString(0)}");
+            }
+            olvaso.Close();
+            Console.WriteLine();
+            lekerdezes.CommandText = "SELECT allergen.nev, count(*)\r\nFROM termek inner JOIN  allergeninfo ON termek.id = allergeninfo.termekId inner JOIN allergen ON allergeninfo.allergenId = allergen.id\r\nGROUP BY allergen.nev desc\r\norder by count(*) DESC limit 3;";
+            olvaso = lekerdezes.ExecuteReader();
+            while(olvaso.Read())
+            {
+                Console.WriteLine($"{olvaso.GetString(0)} {olvaso.GetInt64(1)}");
+            }
+            olvaso.Close();
+            Console.WriteLine();
+            lekerdezes.CommandText = "SELECT termek.nev, termek.ar FROM termek WHERE termek.laktozmentes = 1 and termek.tejmentes = 1 AND termek.tojasmentes = 1 and termek.id not in (select allergeninfo.termekId from allergeninfo) ";
+            olvaso = lekerdezes.ExecuteReader();
+            while (olvaso.Read())
+            {
+                Console.WriteLine($"{olvaso.GetString(0)} {olvaso.GetInt32(1)}");
+            }
+
+            olvaso.Close();
+            Console.WriteLine();
+            lekerdezes.CommandText = "SELECT termek.nev, (termek.ar-100)*12\r\nfrom termek\r\nWHERE termek.nev like \"Paleo%\";";
+            olvaso = lekerdezes.ExecuteReader();
+            while (olvaso.Read())
+            {
+                Console.WriteLine($"{olvaso.GetString(0)} torta fizetendő ár: {olvaso.GetInt64(1)}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
